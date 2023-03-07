3228f5c8c598b40309653f102bbe6bfedfdfdafb
RisaMuraguchi <126639980+RisaMuraguchi@users.noreply.github.com>
2023-03-07 07:56:27 +0000

    課題2削除機能追加

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,7 +1,4 @@
 class BooksController < ApplicationController
-  def new
-    @book = Book.new
-  end
 
   def create
     book = Book.new(book_params)
@@ -11,6 +8,7 @@ class BooksController < ApplicationController
 
   def index
     @books = Book.all
+    @book = Book.new
   end
 
   def show
@@ -20,9 +18,21 @@ class BooksController < ApplicationController
   def edit
     @book = Book.find(params[:id])
   end
-
+  
+  def update
+    book = Book.find(params[:id])
+    book.update(book_params)
+    redirect_to book_path(book.id)
+  end  
+  
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to '/books'
+  end  
+    
   private
-  # ストロングパラメータ
+  
   def book_params
     params.require(:book).permit(:title, :body)
   end
