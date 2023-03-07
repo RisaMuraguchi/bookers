6f2bbf4e09505f21e560db71da48c6f3a063946c
RisaMuraguchi <126639980+RisaMuraguchi@users.noreply.github.com>
2023-03-07 10:11:03 +0000

    課題２　削除機能追加” git commit -m 課題２　削除機能追加”

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,10 +1,13 @@
 class BooksController < ApplicationController
 
   def create
-    book = Book.new(book_params)
-    book.save
-    redirect_to book_path(book.id)
-  end
+    @book = Book.new(book_params)
+    if @book.save
+    redirect_to book_path(@book.id)
+    else
+     render :index
+    end
+  end  
 
   def index
     @books = Book.all
@@ -18,22 +21,23 @@ class BooksController < ApplicationController
   def edit
     @book = Book.find(params[:id])
   end
-  
+
   def update
     book = Book.find(params[:id])
     book.update(book_params)
     redirect_to book_path(book.id)
-  end  
-  
+  end
+
   def destroy
     book = Book.find(params[:id])
     book.destroy
     redirect_to '/books'
-  end  
-    
+  end
+
   private
-  
+
   def book_params
     params.require(:book).permit(:title, :body)
   end
+
 end
